fa9a18175b719a546dd515f6e88ec34f56b21518
tjololo <1145298+tjololo@users.noreply.github.com>
2024-12-19 12:44:18 +0100

    fix go imports, add comments, create test examples

--- modules/azure/container_apps.go
+++ modules/azure/container_apps.go
@@ -2,17 +2,21 @@ package azure
 
 import (
 	"context"
+	"testing"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/appcontainers/armappcontainers/v3"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
+// ManagedEnvironmentExists indicates whether the specified Managed Environment exists.
+// This function would fail the test if there is an error.
 func ManagedEnvironmentExists(t *testing.T, environmentName string, resourceGroupName string, subscriptionID string) bool {
 	exists, err := ManagedEnvironmentExistsE(environmentName, resourceGroupName, subscriptionID)
 	require.NoError(t, err)
 	return exists
 }
 
+// ManagedEnvironmentExistsE indicates whether the specified Managed Environment exists.
 func ManagedEnvironmentExistsE(environmentName string, resourceGroupName string, subscriptionID string) (bool, error) {
 	client, err := CreateManagedEnvironmentsClientE(subscriptionID)
 	if err != nil {
@@ -25,6 +29,15 @@ func ManagedEnvironmentExistsE(environmentName string, resourceGroupName string,
 	return true, nil
 }
 
+// GetManagedEnvironment gets the Managed Environment object
+// This function would fail the test if there is an error.
+func GetManagedEnvironment(t *testing.T, environmentName string, resourceGroupName string, subscriptionID string) *armappcontainers.ManagedEnvironment {
+	env, err := GetManagedEnvironmentE(environmentName, resourceGroupName, subscriptionID)
+	require.NoError(t, err)
+	return env
+}
+
+// GetManagedEnvironmentE gets the Managed Environment object
 func GetManagedEnvironmentE(environmentName string, resourceGroupName string, subscriptionID string) (*armappcontainers.ManagedEnvironment, error) {
 	client, err := CreateManagedEnvironmentsClientE(subscriptionID)
 	if err != nil {
@@ -37,12 +50,15 @@ func GetManagedEnvironmentE(environmentName string, resourceGroupName string, su
 	return &env.ManagedEnvironment, nil
 }
 
+// ContainerAppExists indicates whether the Container App exists for the subscription.
+// This function would fail the test if there is an error.
 func ContainerAppExists(t *testing.T, containerAppName string, resourceGroupName string, subscriptionID string) bool {
 	exists, err := ContainerAppExistsE(containerAppName, resourceGroupName, subscriptionID)
 	require.NoError(t, err)
 	return exists
 }
 
+// ContainerAppExistsE indicates whether the Container App exists for the subscription.
 func ContainerAppExistsE(containerAppName string, resourceGroupName string, subscriptionID string) (bool, error) {
 	client, err := CreateContainerAppsClientE(subscriptionID)
 	if err != nil {
@@ -55,6 +71,15 @@ func ContainerAppExistsE(containerAppName string, resourceGroupName string, subs
 	return true, nil
 }
 
+// GetContainerApp gets the Container App object
+// This function would fail the test if there is an error.
+func GetContainerApp(t *testing.T, containerAppName string, resourceGroupName string, subscriptionID string) *armappcontainers.ContainerApp {
+	app, err := GetContainerAppE(containerAppName, resourceGroupName, subscriptionID)
+	require.NoError(t, err)
+	return app
+}
+
+// GetContainerAppE gets the Container App object
 func GetContainerAppE(environmentName string, resourceGroupName string, subscriptionID string) (*armappcontainers.ContainerApp, error) {
 	client, err := CreateContainerAppsClientE(subscriptionID)
 	if err != nil {
@@ -67,12 +92,15 @@ func GetContainerAppE(environmentName string, resourceGroupName string, subscrip
 	return &app.ContainerApp, nil
 }
 
+// ContainerAppJobExists indicates whether the Container App Job exists for the subscription.
+// This function would fail the test if there is an error.
 func ContainerAppJobExists(t *testing.T, containerAppName string, resourceGroupName string, subscriptionID string) bool {
 	exists, err := ContainerAppJobExistsE(containerAppName, resourceGroupName, subscriptionID)
 	require.NoError(t, err)
 	return exists
 }
 
+// ContainerAppJobExistsE indicates whether the Container App Job exists for the subscription.
 func ContainerAppJobExistsE(containerAppName string, resourceGroupName string, subscriptionID string) (bool, error) {
 	client, err := CreateContainerAppJobsClientE(subscriptionID)
 	if err != nil {
@@ -85,6 +113,15 @@ func ContainerAppJobExistsE(containerAppName string, resourceGroupName string, s
 	return true, nil
 }
 
+// GetContainerAppJob gets the Container App Job object
+// This function would fail the test if there is an error.
+func GetContainerAppJob(t *testing.T, containerAppName string, resourceGroupName string, subscriptionID string) *armappcontainers.Job {
+	app, err := GetContainerAppJobE(containerAppName, resourceGroupName, subscriptionID)
+	require.NoError(t, err)
+	return app
+}
+
+// GetContainerAppJobE gets the Container App Job object
 func GetContainerAppJobE(environmentName string, resourceGroupName string, subscriptionID string) (*armappcontainers.Job, error) {
 	client, err := CreateContainerAppJobsClientE(subscriptionID)
 	if err != nil {
